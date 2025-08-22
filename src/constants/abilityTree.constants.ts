d35a4ca2281ced6351df170e9ebbf37f2bb8516b
Robinator9002 <r.schumi09@gmail.com>
2025-08-22 22:35:53 +0200

    feat: Added NODE_HEIGHT constant for precise snapping calculations

--- src/constants/abilityTree.constants.ts
+++ src/constants/abilityTree.constants.ts
@@ -8,6 +8,9 @@
 // This value is crucial for calculating node positions and rendering tier lines.
 export const TIER_HEIGHT = 180;
 
+// The fixed height of an ability node. This is used for accurate positioning calculations.
+export const NODE_HEIGHT = 80; // Based on .ability-node height in _ability-node.css
+
 // The starting X position for newly created nodes.
 // This helps in consistent placement of nodes when they are first added to the canvas.
 export const NODE_START_X = 200;
