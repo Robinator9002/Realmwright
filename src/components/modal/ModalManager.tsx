1227bbe4bb16c9837412703a2125d5681b403651
Robinator9002 <r.schumi09@gmail.com>
2025-09-18 20:54:21 +0200

    fix(modals): switch on discriminated union property for type safety

--- src/components/modal/ModalManager.tsx
+++ src/components/modal/ModalManager.tsx
@@ -3,7 +3,6 @@
 import { useModal } from '../../context/global/ModalContext';
 import { AlertModal } from './AlertModal';
 import { ConfirmationModal } from './ConfirmationModal';
-// NEW: Import our specialized modal component
 import { LinkLocationModal } from '../specific/Map/Modal/LinkLocationModal';
 
 /**
@@ -11,51 +10,47 @@ import { LinkLocationModal } from '../specific/Map/Modal/LinkLocationModal';
  * It listens to the ModalContext and displays the correct modal based on its state.
  */
 export const ModalManager = () => {
-    const { modalType, modalOptions, hideModal } = useModal();
+    // REWORK: We only need modalOptions and hideModal now.
+    // The modalType is inferred from the options object.
+    const { modalOptions, hideModal } = useModal();
 
-    if (!modalType || !modalOptions) {
+    if (!modalOptions) {
         return null;
     }
 
-    const renderModal = () => {
-        switch (modalType) {
-            case 'alert':
-                return (
-                    <AlertModal isOpen={true} onClose={hideModal} title={modalOptions.title}>
-                        {modalOptions.message}
-                    </AlertModal>
-                );
-            case 'confirmation':
-                return (
-                    <ConfirmationModal
-                        isOpen={true}
-                        onClose={hideModal}
-                        onConfirm={() => {
-                            modalOptions.onConfirm?.();
-                            hideModal();
-                        }}
-                        title={modalOptions.title}
-                        isDanger={modalOptions.isDanger}
-                    >
-                        {modalOptions.message}
-                    </ConfirmationModal>
-                );
-            // NEW: Add the case for our new modal type
-            case 'link-location':
-                return (
-                    <LinkLocationModal
-                        isOpen={true}
-                        onClose={hideModal}
-                        // We need to assert the type of onConfirm here because the generic
-                        // ModalOptions type doesn't know it expects a number. We will fix
-                        // this in the next step by improving the context's type safety.
-                        onConfirm={modalOptions.onConfirm as (locationId: number) => void}
-                    />
-                );
-            default:
-                return null;
-        }
-    };
-
-    return <>{renderModal()}</>;
+    // REWORK: We now switch on the `type` property of the modalOptions object itself.
+    // This allows TypeScript to correctly narrow the type of modalOptions in each case.
+    switch (modalOptions.type) {
+        case 'alert':
+            return (
+                <AlertModal isOpen={true} onClose={hideModal} title={modalOptions.title}>
+                    {modalOptions.message}
+                </AlertModal>
+            );
+        case 'confirmation':
+            return (
+                <ConfirmationModal
+                    isOpen={true}
+                    onClose={hideModal}
+                    onConfirm={() => {
+                        modalOptions.onConfirm();
+                        hideModal();
+                    }}
+                    title={modalOptions.title}
+                    isDanger={modalOptions.isDanger}
+                >
+                    {modalOptions.message}
+                </ConfirmationModal>
+            );
+        case 'link-location':
+            return (
+                <LinkLocationModal
+                    isOpen={true}
+                    onClose={hideModal}
+                    onConfirm={modalOptions.onConfirm}
+                />
+            );
+        default:
+            return null;
+    }
 };
